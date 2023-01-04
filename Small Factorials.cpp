faeb42d00158a13709d90fe9992e0ae7365fba24
renu sri yadav <121473779+22A91A05I3@users.noreply.github.com>
2023-01-04 12:24:58 +0530

    Add files via upload

---
+++ Small Factorials.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++)
+	{
+		int a,f=1;
+		scanf("%d",&a);
+		for(int j=1;j<=a;j++)
+		{
+			f=f*j;
+		}
+		printf("%d\n",f);
+	}
+}
